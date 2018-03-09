d9acfcfd49e24652119014cdf02b2914d3fdf058
Ed Maphis <emaphis85@gmail.com>
2018-03-09 11:53:28 -0500

    Complete day 24 Review - Linked List Challenge

---
+++ src/day_24/Node.java
@@ -0,0 +1,16 @@
+/*
+ * Hacker Rank Day 24 - Java
+ * Review - More Linked Lists
+ */
+package day_24;
+
+
+public class Node {
+    int data;
+    Node next;
+    Node(int d){
+        data=d;
+        next=null;
+    }
+
+}
